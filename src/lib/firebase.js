8508a6558a621f0602cc294ee685c8133cc66c12
Esref <esrefnakkas@email.com>
2026-03-27 09:00:00 +0300

    Firebase config

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,16 +3,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDvvH0wquXY7HvE_j_13rWDoh3T5_a9XE",
-  authDomain: "nakapp-cd89a.firebaseapp.com",
-  projectId: "nakapp-cd89a",
-  storageBucket: "nakapp-cd89a.firebasestorage.app",
+  apiKey: "AIzaSyDvvH0wquXY7HvE_j_l3rWVDoh3T5_a9XE",
+  authDomain: "nakkapp-cd89a.firebaseapp.com",
+  projectId: "nakkapp-cd89a",
+  storageBucket: "nakkapp-cd89a.firebasestorage.app",
   messagingSenderId: "988625639796",
   appId: "1:988625639796:web:d16d24b89bb0cc55cab421",
   measurementId: "G-J0KYQK68DV"
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);
